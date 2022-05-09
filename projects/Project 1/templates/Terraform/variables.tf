fa2c0af77cbc83a0c2eeb1ddc933b23b81484f89
moccolfc <moccolfc@gmail.com>
2022-05-09 09:46:02 +0100

    upload of variables

---
+++ projects/Project 1/templates/Terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "primary_location" {
+  type = string
+  description = "Primary Location"
+  default = "WestUS2"
+}
+
+variable "Web-Prefix" {
+  type = string
+  description = "Web Prefix"
+  default = "Web"
+}
+
+variable "Sql-Prefix" {
+  type = string
+  description = "Sql Prefix"
+  default = "Sql"
+}
+
+variable "Admin-Username" {
+  type = string
+  description = "Admin-Username"
+  default = "sysadmin"
+}
+variable "Admin-Password" {
+  type = string
+  description = "Admin-Password"
+  default = "P@ssw0rd2020"
+}
